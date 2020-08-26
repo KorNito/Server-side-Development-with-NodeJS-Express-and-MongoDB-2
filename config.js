d1c293da43fb5ee5f9c4ce40f77808fd2c564d4a
KorNito <kornelijuspacevicius@gmail.com>
2020-08-26 19:51:03 +0300

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '310938853453401',
+        clientSecret: '37f615253b34353df5e6a207fd18e6ac'
+    }
 }
